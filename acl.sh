8521fcac2ad8974b3247e05a3983f6d1ef19435b
fy-deng <46555482+fy-deng@users.noreply.github.com>
2023-04-17 16:45:02 +0800

    Update acl.sh

--- acl.sh
+++ acl.sh
@@ -3,15 +3,15 @@
 clear
 
 #restore to initial state
-sudo chown root:root /etc/ssl/private/private.key
-sudo chgrp root /etc/ssl/private/
-sudo chmod 600 /etc/ssl/private/private.key
+#sudo chown root:root /etc/ssl/private/private.key
+#sudo chgrp root /etc/ssl/private/
+#sudo chmod 600 /etc/ssl/private/private.key
 
 #Insatll ACL
 sudo apt-get update -y
 sudo apt-get install acl -y
 
-#sudo useradd -r -m -s /bin/false isvc
+sudo useradd -r -m -s /bin/false isvc
 
 sudo setfacl -m u:isvc:rx /etc/ssl/private/
 sudo setfacl -m u:isvc:r /etc/ssl/private/private.key
